950afcaa0e33126251c9c21171000a9555d1aea0
PyTemi <abioduntemitayo2002@gmail.com>
2022-09-11 13:45:20 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,61 +1,43 @@
-/*
- *
- *  * File: 0-positive_or_negative.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
+#include <stdio.h>
 
 #include <stdlib.h>
 
 #include <time.h>
 
-#include <stdio.h>
+/* more headers goes there */
 
 
+
+/* betty style doc for function main goes there */
 
 /**
  *
- *  * main - Prints a random number and states whether
+ *  * main - main function
  *
- *   *        it is positive, negative, or zero.
+ *   * No Description:
  *
- *    *
- *
- *     * Return: Always 0.
+ *    * Return: 0
  *
  *     */
 
 int main(void)
 
 {
-
-		int n;
-
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-
-
-					if (n > 0)
-
-								printf("%d is positive\n", n);
-
-						else if (n < 0)
-
-									printf("%d is negative\n", n);
-
-							else
-
-										printf("%d is zero\n", n);
-
-
-
-								return (0);
-
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+											}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+											}
+	return (0);
 }
