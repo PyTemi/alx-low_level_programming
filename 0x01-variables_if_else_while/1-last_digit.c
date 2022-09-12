35d65afdb462d34d9df7101a16d83350970d37d8
PyTemi <abioduntemitayo2002@gmail.com>
2022-09-11 22:22:08 -0700

    digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
  *
  *   * Auth: Zaabi Mehdi
  *
- *    */
+ */
 
 
 
@@ -26,21 +26,21 @@
  *
  *     * Return: Always 0.
  *
- *      */
+ */
 
 int main(void)
 
 {
 	int n;
-	srand(time(0));
+srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
 	{
-	   printf("Last digit of %d is %d and is greater than 5\n",n, n % 10);
+	   printf("Last digit of %d is %d and is greater than 5\n" , n , n % 10);
 	}
 	else if (n % 10 == 0)
 	{
-		printf("Last digit of %d is 0 and is 0\n",n);
+		printf("Last digit of %d is 0 and is 0\n" , n);
 	}
 	else
 	{
